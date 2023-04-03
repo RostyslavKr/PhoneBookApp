fe89d262a0e5aa7349d30f792152a2d317732adf
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-04-03 20:58:37 +0300

    moved last version phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { getError, getIsLoading } from 'redux/selectors';
+import { Forms } from './Forms/Forms';
+import { Filter } from './Filter/Filter';
+import { Contacts } from './Contacts/Contacts';
+import {
+  Container,
+  ContainerContacts,
+  TitlePhonebook,
+  TitleContacts,
+} from './Container.styled';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <TitlePhonebook>Phonebook</TitlePhonebook>
+      <Forms />
+      <ContainerContacts>
+        <TitleContacts>Contacts</TitleContacts>
+        {isLoading && !error && <b>Request in progress...</b>}
+        <Filter />
+        <Contacts />
+      </ContainerContacts>
+    </Container>
   );
 };
